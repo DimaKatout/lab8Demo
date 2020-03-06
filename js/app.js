32c8470d515d7fe3ca5da70fc53b610931ab296a
DimaKatout <dimakatout97@gmail.com>
2020-03-06 14:03:34 +0200

    a little Demo

---
+++ js/app.js
@@ -0,0 +1,13 @@
+/*var box = document.getElementById('box');
+//type and the action two parameters
+//click when click the box
+box.addEventListener("click",function(){});
+alert('hi');
+
+//box.addEventListener("click",sayHi)
+/*function sayHi(){
+    alert('hi');
+}*/ 
+
+//we will continue the demo of lab 8 on the cats adoption project
+//Lab7Demo
